529d0bab19580512b6f7ba78e074c588f15b8852
Vitalii <ghvitaliidrapaliuk@gmail.com>
2022-05-27 01:10:11 +0300

    add shared folder

--- src/pages/example/ExamplePage.jsx
+++ src/pages/example/ExamplePage.jsx
@@ -1,9 +1,11 @@
-import { SharedExampleBtn } from '../../components/shared/SharedExampleBtn';
+import { SharedExampleBtn } from '../../shared/SharedExampleBtn';
+import { ExampleText } from './components/ExampleText';
 
 export const Example = () => {
   return (
     <>
       <h1>Hello team!</h1>
+      <ExampleText />
       <SharedExampleBtn />
     </>
   );
