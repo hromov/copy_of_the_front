225743d4248c853498da62f9760d0de92986bd56
Serhii Hromov <melifarowow@gmail.com>
2022-05-28 00:19:07 +0300

    some changes to check aws

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Example } from './pages/example/ExamplePage';
 function App() {
   return (
     <div className='App'>
+      <h1>Some changes to check deploy</h1>
       <Example />
     </div>
   );
